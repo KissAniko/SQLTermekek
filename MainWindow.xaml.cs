298406e9a84b84fcd0e4d82f2fa60dd9cb79ac4b
Kiss Anikó <126045521+KissAniko@users.noreply.github.com>
2023-05-02 10:18:27 +0200

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,6 @@
-﻿using MySqlConnector;
+﻿using MySqlConnector;  // MySqlConnector telepítése
 using System;
-using System.Collections.Generic;
+using System.Collections.Generic;  
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
